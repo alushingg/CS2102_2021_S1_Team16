5d10f0c6b4b0051a49455e00a0f03408a08f077e
CheyanneSim <CheyanneSim@users.noreply.github.com>
2020-10-30 20:25:27 +0800

    Merging

--- app/database/database.js
+++ app/database/database.js
@@ -1,12 +1,16 @@
 const postgre = require('pg');
 require('dotenv').config();
 
-const db = new postgre.Pool({
+/*const db = new postgre.Pool({
     user:process.env.LOCAL_PG_USER,
     host:process.env.LOCAL_PG_HOST,
     database:process.env.PG_DB,
     password:process.env.LOCAL_PG_PW,
     port:process.env.PG_PORT,
+});*/
+
+const db = new postgre.Pool({
+    connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = db;
